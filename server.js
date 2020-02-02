25795bb6d3590ab9e7567a5a903d733d357b3c93
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-02-02 11:08:55 -0600

    route is tested using postman

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('app is working');
+});
+
+app.post('/signin', (req, res) => {
+  res.json('signin');
+});
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
